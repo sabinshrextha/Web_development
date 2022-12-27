4c496badfc84520f5e4dce095873895843c2c668
sabinshrextha <110049712+sabinshrextha@users.noreply.github.com>
2022-12-27 10:29:24 +0545

    Add files via upload

---
+++ console.js
@@ -0,0 +1,3 @@
+console.log('Hello, Its me')
+n = ("shiril")
+console.log(n)
---
+++ oddEven.js
@@ -0,0 +1,7 @@
+var c = prompt("Enter the number")
+
+if (c%2==0){
+    document.getElementById("demo").innerHTML="Even";
+} else{
+    document.getElementById("demo").innerHTML= "Odd";
+}
